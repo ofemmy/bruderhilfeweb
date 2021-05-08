80a941a8f5d172396145562a9235580c161ff616
Oladayo Fagbemi <oofagbemi@outlook.com>
2021-05-08 06:21:32 +0200

    current project page done

---
+++ lib/current.ts
@@ -0,0 +1,36 @@
+export const budgetListings = [
+  {
+    amountNGN: "NGN 1,100,000",
+    amountUSD: "$2686.79",
+    location1: "Kemta Village,",
+    location2: "Ogun State",
+    items: [
+      "Training of women on financial literacy",
+      "Provision of revolving loan, monitoring and evaluation.",
+      "Activity trainings.",
+    ],
+  },
+  {
+    amountNGN: "NGN 1,100,000",
+    amountUSD: "$2686.79",
+    location1: "Ajegunle Community,",
+    location2: " Ikorodu, Lagos",
+    items: [
+      "Procurement of mobile technology tools to drive rural women socio-economic activities.",
+      "Revolving loan scheme.",
+      "Monthly monitoring and evaluation of project activities.",
+    ],
+  },
+  {
+    amountNGN: "NGN 1,700,000",
+    amountUSD: "$4152.32",
+    location1: "Kwana-Makeri Village,",
+    location2: "Niger State",
+    items: [
+      "Training of women on socio-economic skills and business that thrives in their context.",
+      "Financial empowerment grant- Kwana-Makeri Community.",
+      "Procurement of destoning machine for the Women Cooperative Society.",
+      "Monthly monitoring and evaluation of the utilisation of the project.",
+    ],
+  },
+];
